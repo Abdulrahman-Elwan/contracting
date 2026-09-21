96d3e450a163b0d2572cbe8f4600f264e4cd4d55
Abdulrahman-Elwan <137184416+Abdulrahman-Elwan@users.noreply.github.com>
2026-09-21 16:15:29 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,20 +42,42 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ar" dir="rtl" className={tajawal.variable}>
-
       <head>
-        <!-- Google Tag Manager -->
-        <script>(function (w, d, s, l, i) {
-            w[l] = w[l] || []; w[l].push({
-              'gtm.start':
-                new Date().getTime(), event: 'gtm.js'
-            }); var f = d.getElementsByTagName(s)[0],
-              j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-          })(window, document, 'script', 'dataLayer', 'GTM-PPL42R68');</script>
-        <!-- End Google Tag Manager -->
+        <Script id="google-tag-manager">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({
+                'gtm.start': new Date().getTime(),
+                event:'gtm.js'
+              });
+
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),
+                  dl=l!='dataLayer'?'&l='+l:'';
+
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PPL42R68');
+          `}
+        </Script>
       </head>
-    <body className="font-arabic bg-white text-navy antialiased">
+
+      <body className="font-arabic bg-white text-navy antialiased">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PPL42R68"
+            height="0"
+            width="0"
+            style={{
+              display: "none",
+              visibility: "hidden",
+            }}
+          />
+        </noscript>
+
         {children}
       </body>
     </html>
