eee806bfe1593dadba6462d07de15abc03ecfd32
Abdulrahman-Elwan <137184416+Abdulrahman-Elwan@users.noreply.github.com>
2026-09-21 15:52:16 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,23 +46,13 @@ export default function RootLayout({
         {children}
       </body>
 
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=AW-18455231627"
-        strategy="afterInteractive"
-      />
-
-      <Script id="google-ads-tag" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-
-          function gtag() {
-            dataLayer.push(arguments);
-          }
-
-          gtag('js', new Date());
-          gtag('config', 'AW-18455231627');
-        `}
-      </Script>
+      <!-- Google Tag Manager -->
+      <Script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-PPL42R68');</Script>
+      <!-- End Google Tag Manager -->
     </html>
   );
 }
